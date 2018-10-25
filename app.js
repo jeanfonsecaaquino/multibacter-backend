9f7fb0f1747a829b68524426778d6e165e687dcf
Jean Fonseca <jean.fonseca.aquino@gmail.com>
2018-10-25 00:05:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ server.get('/', respond);
 server.get('/qrcode/:qrCode', conteudoCardController.validaQRCOde.bind(this));
 server.get('/conteudo/:tipoConteudo', conteudoCardController.retornarConteudo.bind(this))
 
-server.listen(8080, function () {
+server.listen(8081, function () {
     console.log('%s listening at %s', server.name, server.url);
 });
